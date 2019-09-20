eec77a4032503b4e0ba41ac16764f56176f0aad5
zi-klon <zi-klon@yandex.ru>
2019-09-20 14:20:55 +0500

    Настроен webpack, установлен Babel

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+    entry: { main: './src/index.js' },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js'
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            use: { loader: "babel-loader" },
+            exclude: /node_modules/
+        }]
+    }
+}
